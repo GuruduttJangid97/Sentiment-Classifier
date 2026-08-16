24afdeea90c8f97fed3fa3bdbe99a4aa4c675f4f
GuruduttJangid97 <Gurudutt.jangid@gmail.com>
2026-08-16 10:59:42 +0530

    Function implemented and tested

--- main.py
+++ main.py
@@ -3,10 +3,32 @@ from sentiment import get_pos, get_neg
 
 with open("assets/project_twitter_data.csv") as file:
     reader = csv.DictReader(file)
-    tweet_text = ""
-    for row in reader:
-        tweet_text += row['tweet_text']
-    positive_score = get_pos(tweet_text)
-    negative_score = get_neg(tweet_text)
-    print(positive_score)
-    print(negative_score)
+
+    with open("resulting_data.csv", mode="w", newline="", encoding="utf-8") as ouput_file:
+        writer = csv.writer(ouput_file)
+
+        writer.writerow([
+            "Number of Retweets",
+            "Number of Replies",
+            "Positive Score",
+            "Negative Score",
+            "Net Score"
+        ])
+
+        for row in reader:
+            Tweet_text = row['tweet_text']
+            Retweet_count = int(row['retweet_count'])
+            Reply_count = int(row['reply_count'])
+
+            Positive_Score = get_pos(Tweet_text)
+            Negative_Score = get_neg(Tweet_text)
+
+            Net_Score = Positive_Score - Negative_Score
+
+            writer.writerow([
+                Retweet_count,
+                Reply_count,
+                Positive_Score,
+                Negative_Score,
+                Net_Score
+            ])
